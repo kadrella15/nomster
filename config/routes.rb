d4cfbe4a75064e3b4cecc878fefaca0676be4b75
kadrella <kadrellall@icloud.com>
2019-10-08 21:14:18 +0000

    Lesson 39 part 4 upload photo button is next to edit button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'places#index'
-  resources :places do
+    resources :places do
     resources :comments, only: :create
     resources :photos, only: :create
   end
